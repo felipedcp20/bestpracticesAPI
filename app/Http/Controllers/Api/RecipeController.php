a205e312c7060fa63ba0640f22e92ba9f0e9410b
feli rodriguez <felipeandres.rodriguez@mercadolibre.com.co>
2023-11-18 12:16:43 -0500

    coherence and consitence in resource and collections

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -5,36 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Resources\RecipeResource;
+
 use App\Models\Recipe;
 
 class RecipeController extends Controller
 {
     public function index()
     {
-        return Recipe::with('category', "tags", "user")->get();
-    }
-
-    public function store(Request $request)
-    {
-        $recipe = Recipe::create($request->all());
-        return $recipe;
+        $recipes = Recipe::with('category', "tags", "user")->get();
+        return RecipeResource::collection($recipes);
     }
 
     public function show(Recipe $recipe)
     {
-        return $recipe->load('category', "tags", "user");
-    }
-
-    public function update(Request $request, Recipe $recipe)
-    {
-        $recipe->update($request->all());
-        return $recipe;
-    }
-    
-    public function destroy(Recipe $recipe)
-    {
-        $recipe->delete();
-        return $recipe;
+        $recipe = $recipe->load('category', "tags", "user");
+        return new RecipeResource($recipe);  
     }
     
 }
--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Resources\TagResource;
+
 use App\Models\Tag;
 
 class TagController extends Controller
 {
     public function index()
     {
-        return \App\Models\Tag::with('recipes')->get();
+        $tags = Tag::with('recipes.category', 'recipes.tags', 'recipes.user')->get();
+
+        return TagResource::collection($tags);
     }
 
     public function show(Tag $tag)
     {
-        return $tag->load('recipes');
+        $tags = $tag->load('recipes.category', 'recipes.tags', 'recipes.user');
+        
+        return new TagResource($tags);
     }
 }
